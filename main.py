74b9972549fd4b8a8ea6dca3828df7c8030ac5b9
Boris Sachet <boris.sachet@gmail.com>
2024-09-02 17:14:40 +0200

    fix: shutted down TiffImagePlugin logger

--- main.py
+++ main.py
@@ -84,6 +84,6 @@ def main():
 if __name__ == '__main__':
     sys.path.append("/var/packages/MediaServer/target/bin/ffmpeg")
     sys.path.append("/var/packages/MediaServer/target/bin/ffprobe")
-    logging.getLogger("TiffImagePlugin").setLevel(logging.INFO)
-    logging.getLogger("TiffImagePlugin").propagate = False
+    logging.getLogger("PIL.TiffImagePlugin").setLevel(logging.INFO)
+    logging.getLogger("PIL.TiffImagePlugin").propagate = False
     main()
